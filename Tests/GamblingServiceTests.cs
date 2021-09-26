98c095764533c557bed19d17be81961cbae98b07
Jürgen Reiser <juergen.reiser@neo-q.de>
2021-09-26 13:58:53 +0200

    Add gambling service + password validator tests

---
+++ Tests/GamblingServiceTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Threading.Tasks;
+using FakeItEasy;
+using GamblingGame.Domain.Enums;
+using GamblingGame.Domain.Exceptions;
+using GamblingGame.Domain.Interfaces;
+using GamblingGame.Domain.Models;
+using GamblingGame.Domain.Services;
+using Xunit;
+
+namespace Tests
+{
+    public class GamblingServiceTests
+    {
+        private readonly GamblingService _classUnderTest;
+        private readonly IAuthenticateContext _authenticateContextFake;
+        private readonly IAccountRepository _accountRepositoryFake;
+        private readonly ILotteryWheel _lotteryWheelFake;
+
+        public GamblingServiceTests()
+        {
+            _authenticateContextFake = A.Fake<IAuthenticateContext>();
+            _accountRepositoryFake = A.Fake<IAccountRepository>();
+            _lotteryWheelFake = A.Fake<ILotteryWheel>();
+            _classUnderTest = new GamblingService(_authenticateContextFake, _accountRepositoryFake, _lotteryWheelFake);
+        }
+
+        [Fact]
+        public async Task GambleAsync_WinTip_ProperResultReturned()
+        {
+            // Arrange
+            const int betPoints = 250;
+            const int tip = 7;
+            var account = new Account
+            {
+                Points = 1000
+            };
+            A.CallTo(() => _accountRepositoryFake.GetByUserIdAsync(A<Guid>.Ignored)).Returns(account);
+            A.CallTo(() => _lotteryWheelFake.GetRandomNumber()).Returns(7);
+
+            // Act
+            var result = await _classUnderTest.GambleAsync(betPoints, tip);
+
+            // Assert
+            Assert.Equal(3250, result.AccountPoints);
+            Assert.Equal("+2250", result.PointsString);
+            Assert.Equal(GambleStatus.Won, result.Status);
+            A.CallTo(() => _accountRepositoryFake.UpdateAsync(account)).MustHaveHappenedOnceExactly();
+        }
+
+        [Fact]
+        public async Task GambleAsync_LoseTip_ProperResultReturned()
+        {
+            // Arrange
+            const int betPoints = 250;
+            const int tip = 7;
+            var account = new Account
+            {
+                Points = 1000
+            };
+            A.CallTo(() => _accountRepositoryFake.GetByUserIdAsync(A<Guid>.Ignored)).Returns(account);
+            A.CallTo(() => _lotteryWheelFake.GetRandomNumber()).Returns(5);
+
+            // Act
+            var result = await _classUnderTest.GambleAsync(betPoints, tip);
+
+            // Assert
+            Assert.Equal(750, result.AccountPoints);
+            Assert.Equal("-250", result.PointsString);
+            Assert.Equal(GambleStatus.Lost, result.Status);
+            A.CallTo(() => _accountRepositoryFake.UpdateAsync(account)).MustHaveHappenedOnceExactly();
+        }
+
+        [Fact]
+        public async Task GambleAsync_OverdraftTip_ExceptionThrown()
+        {
+            // Arrange
+            const int betPoints = 1100;
+            const int tip = 7;
+            var account = new Account
+            {
+                Points = 1000
+            };
+            A.CallTo(() => _accountRepositoryFake.GetByUserIdAsync(A<Guid>.Ignored)).Returns(account);
+
+            // Act + Assert
+            await Assert.ThrowsAsync<OverdraftException>(() => _classUnderTest.GambleAsync(betPoints, tip));
+            A.CallTo(() => _accountRepositoryFake.UpdateAsync(account)).MustNotHaveHappened();
+        }
+    }
+}
---
+++ Tests/PasswordValidatorTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq;
+using GamblingGame.Domain.Services;
+using Xunit;
+
+namespace Tests
+{
+    public class PasswordValidatorTests
+    {
+        private readonly PasswordValidator _classUnderTest;
+
+        public PasswordValidatorTests()
+        {
+            _classUnderTest = new PasswordValidator();
+        }
+
+        [Fact]
+        public void Validate_ValidInput_NoErrorsReturned()
+        {
+            // Arrange
+            const string password = "abcDefg!";
+
+            // Act
+            var errors = _classUnderTest.Validate(password);
+
+            // Assert
+            Assert.Equal(0, errors.Count);
+        }
+
+        [Fact]
+        public void Validate_InvalidLength_ErrorReturned()
+        {
+            // Arrange
+            const string password = "cDfg!";
+
+            // Act
+            var errors = _classUnderTest.Validate(password);
+
+            // Assert
+            Assert.Equal(1, errors.Count);
+            Assert.Contains("at least 6 characters", errors.First());
+        }
+
+        [Fact]
+        public void Validate_InvalidNoUpperCase_ErrorReturned()
+        {
+            // Arrange
+            const string password = "abcdefg!";
+
+            // Act
+            var errors = _classUnderTest.Validate(password);
+
+            // Assert
+            Assert.Equal(1, errors.Count);
+            Assert.Contains("an upper letter", errors.First());
+        }
+
+        [Fact]
+        public void Validate_InvalidNoSpecialCharacter_ErrorReturned()
+        {
+            // Arrange
+            const string password = "abcDefgw";
+
+            // Act
+            var errors = _classUnderTest.Validate(password);
+
+            // Assert
+            Assert.Equal(1, errors.Count);
+            Assert.Contains("a special character", errors.First());
+        }
+    }
+}
